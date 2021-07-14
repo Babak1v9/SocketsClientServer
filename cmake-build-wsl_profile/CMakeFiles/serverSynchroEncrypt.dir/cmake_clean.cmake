a15f39a501d3060040bd5c718fa2a0507360d923
Babak | Lukas <55434617+Babak1v9@users.noreply.github.com>
2021-07-14 12:27:09 +0200

    Add files via upload

---
+++ cmake-build-wsl_profile/CMakeFiles/serverSynchroEncrypt.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/serverSynchroEncrypt.dir/serverSynchroEncrypt.cpp.o"
+  "serverSynchroEncrypt"
+  "serverSynchroEncrypt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/serverSynchroEncrypt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-wsl_profile/CMakeFiles/vs.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vs.dir/client.cpp.o"
+  "CMakeFiles/vs.dir/server.cpp.o"
+  "vs"
+  "vs.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
